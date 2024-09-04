a248f561de146f65490bfbe4823a77abfe84d4bf
krolik131 <kostya-leteha@mail.ru>
2024-09-04 14:53:20 +0300

    Add files via upload

---
+++ lesson_homework.py
@@ -0,0 +1,9 @@
+#1st program
+print(9 ** 0.5 * 5)
+#2st program
+print(9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print((2 * 2 + 2) == 2 * (2 + 2))
+#4th program
+x = float('123.456')
+print(int(x*10%10))
